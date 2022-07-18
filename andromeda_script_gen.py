dee29e24b2c3bc5a88408fa48c56def0ccef68eb
Erik Carlson <erikvcarlson@gmail.com>
2022-07-18 19:47:45 -0400

    change of structure

--- andromeda_script_gen.py
+++ andromeda_script_gen.py
@@ -2,45 +2,60 @@ import pandas as pd
 import os
 import shutil
 import sys
+#this script is run in the /data/astrolab/Carlson/Astro/VLASS_Data/Rnd2 folder
+Sources = ['J0740+3756','J0912+5650','J0945+1726', 'J1007+5802','J1049+6406','J1214+1631','J1253+4633','J1350+1740','J1548+1935','J1558+1304','J1635+2547', 'J1643+1235','J0744+1825','J0930+1859', 'J1002+1916', 'J1010+1851', 'J1207+1127', 'J1246+2245',  'J1344+2852',  'J1416+6205', 'J1553+1401', 'J1612+2029', 'J1642+1848']
+for source in Sources:
+    J_Name_of_Source = source
+
+    allsourcedf = pd.read_csv('/data/astrolab/Carlson/Astro/VLASS_Data/Large_VLASS_Automation/No_VLA_Obs_Data_and_MS.csv')
+    allsourcedf['Coords'] = allsourcedf['Coords'].str.replace(' ',':',n=3)
+    allsourcedf['Coords'] = allsourcedf['Coords'].str.replace(' ','.')
+    allsourcedf['Coords'] = allsourcedf['Coords'].str.replace(':+',' +',regex=False)
 
 
-J_Name_of_Source = str(sys.argv[1])
+    sig_source_df = allsourcedf[allsourcedf['J-Name'] == J_Name_of_Source]
 
-allsourcedf = pd.read_csv('/data/astrolab/Carlson/Astro/VLASS_Data/Large_VLASS_Automation/No_VLA_Obs_Data_and_MS.csv')
-allsourcedf['Coords'] = allsourcedf['Coords'].str.replace(' ',':',n=3)
-allsourcedf['Coords'] = allsourcedf['Coords'].str.replace(' ','.')
-allsourcedf['Coords'] = allsourcedf['Coords'].str.replace(':+',' +',regex=False)
+    os.chdir('/data/astrolab/Carlson/Astro/VLASS_Data/Rnd2' +  J_Name_of_Source)
+    os.makedirs("working")
 
+    shutil.copyfile('/data/astrolab/Carlson/Astro/VLASS_Data/Large_VLASS_Automation/command_script.py','working/command_script.py')
+    MS_Name = J_Name_of_Source + '_split.ms'
 
-sig_source_df = allsourcedf[allsourcedf['J-Name'] == J_Name_of_Source]
+    shutil.move(MS_Name,'working/'+ MS_Name)
 
+    with open('working/command_script.py', 'r') as file :
+        filedata = file.read()
 
-os.makedirs("working")
+    filedata = filedata.replace('example.ms',MS_Name)
 
-shutil.copyfile('/data/astrolab/Carlson/Astro/VLASS_Data/Large_VLASS_Automation/command_script.py','working/command_script.py')
-MS_Name = J_Name_of_Source + '_split.ms'
-
-shutil.move(MS_Name,'working/'+ MS_Name)
-
-with open('working/command_script.py', 'r') as file :
-    filedata = file.read()
-
-filedata = filedata.replace('example.ms',MS_Name)
-
-with open('working/command_script.py', 'w') as file :
-    file.write(filedata)
+    with open('working/command_script.py', 'w') as file :
+        file.write(filedata)
 
 
-filedata = filedata.replace('00:00:00.0 +00.00.00.0',sig_source_df['Coords'].to_string())
+    filedata = filedata.replace('00:00:00.0 +00.00.00.0',sig_source_df['Coords'].to_string())
 
-shutil.copyfile('/data/astrolab/Carlson/Astro/VLASS_Data/Large_VLASS_Automation/command_script.py','working/SEIP_parameter.list')
+    shutil.copyfile('/data/astrolab/Carlson/Astro/VLASS_Data/Large_VLASS_Automation/command_script.py','working/SEIP_parameter.list')
+    shutil.copyfile('/data/astrolab/Carlson/Astro/VLASS_Data/Large_VLASS_Automation/run_SE.sh','working/run_SE.sh')
 
 
-with open('working/command_script.py', 'r') as file :
-    filedata = file.read()
 
-filedata = filedata.replace('00:00:00.0 +00.00.00.0',sig_source_df['Coords'].to_string())
-filedata = filedata.replace('J0000+0000',J_Name_of_Source)
+    with open('working/command_script.py', 'r') as file :
+        filedata = file.read()
 
-with open('working/command_script.py', 'w') as file :
-    file.write(filedata)
+    filedata = filedata.replace('00:00:00.0 +00.00.00.0',sig_source_df['Coords'].to_string())
+    filedata = filedata.replace('J0000+0000',J_Name_of_Source)
+
+    with open('working/command_script.py', 'w') as file :
+        file.write(filedata)
+
+
+    with open('working/run_SE.sh', 'r') as file :
+        filedata = file.read()
+
+    filedata = filedata.replace('J0000+0000',J_Name_of_Source)
+
+    with open('working/run_SE.sh', 'w') as file :
+        file.write(filedata)
+    
+    os.system('sbatch working/run_SE.sh')
+    os.chdir('/data/astrolab/Carlson/Astro/VLASS_Data/Rnd2')
--- run_SE.sh
+++ run_SE.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 #SBATCH --cpus-per-task=4   # Amount of memory needed by each process (ppn) in the job.
-#SBATCH -D /data/astrolab/Carlson/Astro/VLASS_Data/Rnd2/J0000+0000 # Working directory (PBS_O_WORKDIR) set to your Lustre area
+#SBATCH -D /data/astrolab/Carlson/Astro/VLASS_Data/Rnd2/J0000+0000/working/ # Working directory (PBS_O_WORKDIR) set to your Lustre area
 #SBATCH --mem=64GB
 #SBATCH --mail-type=END,FAIL                 # Send mail on begin, end and abort
 #SBATCH --mail-user=erikvcarlson@uri.edu
 
 module load Xvfb/1.20.13-GCCcore-11.2.0
 module load CASA
-module load pandas/1.1.2-foss-2020a-Python-3.8.2
-module load Python/3.8.2-GCCcore-9.3.0
-
-#run the script generator
-python3 andromeda_script_gen.py J0000+0000 
 
 
 # casa's python requires a DISPLAY for matplot, so create a virtual X server
-xvfb-run -a casa --pipeline --nogui --nologger -c working/command_script.py
+xvfb-run -a casa --pipeline --nogui --nologger -c command_script.py
